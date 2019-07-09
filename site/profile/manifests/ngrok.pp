036443d1478fa7133758fb0187919c46e976eb9e
LightlyH <lilyhu.dev@gmail.com>
2019-07-08 22:39:31 -0400

    Update ngrok.pp

--- site/profile/manifests/ngrok.pp
+++ site/profile/manifests/ngrok.pp
@@ -1,6 +1,7 @@
 class profile::ngrok {  	
   class { 'ngrok':	
     authtoken       => '43vtiYGuNzogCf3Kh5wtF_7yaxJTARNgFn9Mvp4vA7f',	
+    service_manage => true,
   }	
 
   ngrok::tunnel { 'webhook':	
