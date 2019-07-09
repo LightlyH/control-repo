f43ef104400b342dadfd19b6c9efedbeb87d3785
LightlyH <lilyhu.dev@gmail.com>
2019-07-08 22:07:49 -0400

    Update ngrok.pp

--- site/profile/manifests/ngrok.pp
+++ site/profile/manifests/ngrok.pp
@@ -1,10 +1,1 @@
-class profile::ngrok {  
-  class { 'ngrok':
-    authtoken       => '43vtiYGuNzogCf3Kh5wtF_7yaxJTARNgFn9Mvp4vA7f',
-  }
 
-  ngrok::tunnel { 'webhook':
-    proto => 'tcp',
-    addr  => '8088',
-  }
-}
